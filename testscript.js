d401b1138b377769001d41bb242dce19f16e958b
Stianh11 <stian.h11.kodehode@gmail.com>
2025-01-21 18:12:17 +0100

    Update removed testscript.js

--- testscript.js
+++ testscript.js
@@ -1,15 +1,1 @@
-const form = document.querySelector("form");
-const card_1 = document.querySelector(".container");
-const card_2 = document.querySelector(".pop-ups");
-const dismiss = document.querySelector("#dismiss");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  card_1.classList.add("hide");
-  card_2.classList.remove("hide");
-});
-
-dismiss.addEventListener("click", () => {
-  card_1.classList.remove("hide");
-  card_2.classList.add("hide");
-})
